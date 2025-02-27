96871a95b6d297e3681ceeb9c6552d7ab0bc5025
René Bod <rene.bod@mac.com>
2025-02-27 11:41:01 +0100

    code to src

--- fastapi/src/dbfill.py
+++ fastapi/src/dbfill.py
@@ -5,7 +5,8 @@ import pytz
 
 def now():
     now = datetime.now(pytz.timezone('Europe/Amsterdam'))
-    return datetime.strftime(now, '%Y-%m-%d-%H:%M:%S')
+    # return datetime.strftime(now, '%Y-%m-%d-%H:%M:%S')
+    return now.isoformat()
 
 def getdb(dbname, **kwargs):
     user = "admin"
